20f6c8dec1e343c0c0abb0ca565d13b876ffc624
Firuziddin Najimov <fnajimovv@gmail.com>
2026-05-29 14:35:27 +0500

    all done

--- Vention/task1.sh
+++ Vention/task1.sh
@@ -1,103 +1,92 @@
 #!/bin/bash
+echo -e "\n===== LAB 3.2 =====\n"
 
-set -eu
+echo -e "1. Local DNS Resolution\n"
+echo '/etc/hosts'
+echo '35.223.222.126 myapp.local'
+echo 'ping myapp.local'
 
-echo -e "===== LAB 2.1 =====\n"
+echo -e "\n2. SSH Local Port Forwarding\n"
+echo 'On GCP VM:'
+echo 'python3 -m http.server 9000 --bind 127.0.0.1'
 
-echo -e "1. Users and Groups\n"
-echo 'sudo groupadd ops'
-echo 'sudo groupadd developer'
-echo 'sudo useradd dev1'
-echo 'sudo useradd dev2'
-echo 'sudo usermod -aG developer dev1'
-echo 'sudo usermod -aG developer dev2'
-echo 'sudo usermod -aG ops deployer'
+echo -e "\nOn local machine:"
+echo 'ssh -L 8080:127.0.0.1:9000 ksan@35.223.222.126'
 
-echo -e "\n1.2 Sudo permissions\n"
-echo 'sudo visudo -f /etc/sudoers.d/deployer'
-echo 'deployer ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart *'
+echo -e "\nOpen in browser:"
+echo 'http://localhost:8080'
 
-echo -e "\n2. SSH Hardening\n"
-echo 'sudo nano /etc/ssh/sshd_config'
-echo 'PermitRootLogin no'
-echo 'PasswordAuthentication no'
-echo 'PubkeyAuthentication yes'
+echo -e "\n3. Self-Signed SSL Certificate\n"
+echo 'sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt'
 
-echo -e "\n3. UFW Firewall\n"
-echo 'sudo ufw allow 22/tcp'
-echo 'sudo ufw allow 80/tcp'
-echo 'sudo ufw allow 443/tcp'
-echo 'sudo ufw enable'
+echo -e "\nConfigure nginx:"
+echo 'sudo nano /etc/nginx/sites-available/default'
 
-echo -e "\n4. Sysctl Configuration\n"
-echo 'sysctl -a'
-echo 'sudo nano /etc/sysctl.d/lab2-custom.conf'
-echo 'net.ipv4.ip_forward = 1'
-echo 'vm.swappiness = 10'
-echo 'net.core.somaxconn = 65535'
-echo 'sudo sysctl --system'
-
-echo -e "\n5. Logrotate Configuration\n"
-echo 'cd /etc/logrotate.d/'
-echo 'nano app_logs.conf'
-echo '/opt/app/logs/*.log {'
-echo '    weekly'
-echo '    rotate 4'
-echo '    compress'
-echo '    delaycompress'
+echo 'server {'
+echo '    listen 443 ssl;'
+echo '    server_name _;'
+echo '    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;'
+echo '    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;'
+echo '    location / {'
+echo '        root /var/www/html;'
+echo '        index index.html index.htm;'
+echo '    }'
 echo '}'
 
-echo -e "\n===== LAB 2.2 =====\n"
+echo -e "\nPrepare test page:"
+echo 'sudo cp /var/www/html/index.nginx-debian.html /var/www/html/index.html'
 
-echo -e "1. Health Check Script\n"
-echo 'cd /opt/healthcheck/'
-echo 'nano check.sh'
-echo 'echo -e "\n Total Disk usage\n"'
-echo "df -h | awk '{print \$1, \$2, \$4}' | column -t"
-echo 'echo -e "\n\n"'
-echo "sudo du -ah --max-depth=1 / 2>&1 | sort -hr | awk '!/du: cannot/{print \$0}'"
-echo 'echo -e "\n Total Memory usage\n"'
-echo "sudo free -h | awk '{print \$1, \$2, \$3}' | column -t"
+echo -e "\nRestart nginx:"
+echo 'sudo systemctl restart nginx'
 
-echo -e "\n2. Systemd Service\n"
-echo 'sudo nano /etc/systemd/system/healthcheck.service'
-echo '[Unit]'
-echo 'Description=HealthCheck Service shows du df free'
-echo '[Service]'
-echo 'Type=oneshot'
-echo 'ExecStart=/opt/healthcheck/check.sh'
-echo 'User=root'
+echo -e "\nTest HTTPS:"
+echo 'https://35.223.222.126'
 
-echo -e "\n3. Systemd Timer\n"
-echo 'sudo nano /etc/systemd/system/healthcheck.timer'
-echo '[Unit]'
-echo 'Description=Run HealthCheck every 5 min'
-echo '[Timer]'
-echo 'OnCalendar=*:0/5'
-echo '[Install]'
-echo 'WantedBy=timers.target'
+echo -e "\n4. Network Capture\n"
+echo 'sudo tcpdump -i any port 80 -w /tmp/http_traffic.pcap'
 
-echo -e "\n4. Timer Activation\n"
-echo 'sudo systemctl daemon-reload'
-echo 'sudo systemctl enable --now healthcheck.timer'
-echo 'systemctl status healthcheck.timer'
-echo 'sudo journalctl -u healthcheck.service'
+echo -e "\n5. Network Segmentation\n"
+echo 'App Servers: 10.0.0.0/24'
+echo 'Hosts: 10.0.0.1 - 10.0.0.254'
+echo 'Broadcast: 10.0.0.255'
 
-echo -e "\n5. LVM Setup\n"
-echo 'sudo pvcreate /dev/sdb'
-echo 'sudo vgcreate data-vg /dev/sdb'
-echo 'sudo lvcreate -L 6G -n app-data data-vg'
-echo 'sudo lvcreate -L 4G -n app-logs data-vg'
-echo 'sudo mkfs.ext4 /dev/data-vg/app-data'
-echo 'sudo mkfs.xfs /dev/data-vg/app-logs'
-echo 'sudo mkdir -p /mnt/app/data'
-echo 'sudo mkdir -p /mnt/app/logs'
-echo 'sudo mount /dev/data-vg/app-data /mnt/app/data'
-echo 'sudo mount /dev/data-vg/app-logs /mnt/app/logs'
+echo ''
+echo 'DMZ: 10.0.1.0/26'
+echo 'Hosts: 10.0.1.1 - 10.0.1.62'
+echo 'Broadcast: 10.0.1.63'
 
-echo -e "\n6. LVM Expansion\n"
-echo 'sudo pvcreate /dev/sdc'
-echo 'sudo vgextend data-vg /dev/sdc'
-echo 'sudo lvextend -L +2G /dev/data-vg/app-data -r'
+echo ''
+echo 'Management: 10.0.1.64/27'
+echo 'Hosts: 10.0.1.65 - 10.0.1.94'
+echo 'Broadcast: 10.0.1.95'
 
-echo -e "\n===== END OF LAB =====\n"
+echo ''
+echo 'DBs: 10.0.1.96/28'
+echo 'Hosts: 10.0.1.97 - 10.0.1.110'
+echo 'Broadcast: 10.0.1.111'
+
+echo -e "\n6. Simple Repository\n"
+echo 'cd /opt/lab3-repo/'
+echo 'echo "hello-anyops" | sudo tee any-ops.rpm'
+echo 'sudo dpkg-scanpackages . /dev/null > Packages'
+
+echo -e "\nNginx configuration:"
+echo 'listen 80;'
+echo 'root /opt/lab3-repo;'
+
+echo -e "\n7. Network Utilities\n"
+echo 'ethtool'
+echo 'netplan apply'
+echo 'nmcli'
+
+echo -e "\n8. Background SSH Tunnel\n"
+echo 'ssh -FnL LOCAL_PORT:REMOTE_HOST:REMOTE_PORT USER@SERVER'
+
+echo -e "\n9. Git Topics\n"
+echo 'Remove .env from history'
+echo 'Git stages'
+echo 'git rebase'
+echo 'git reset'
+echo 'git restore'
+
+echo -e "\n===== END OF LAB 3.2 =====\n"
