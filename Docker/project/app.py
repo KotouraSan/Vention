56156751c5cfd94d45149822e9f336b80b342cab
Firuziddin Najimov <fnajimovv@gmail.com>
2026-06-05 18:20:10 +0500

    task 2 done

--- Docker/project/app.py
+++ Docker/project/app.py
@@ -1,6 +1,32 @@
+import os
+
 from flask import Flask, jsonify
+import psycopg
 
 app = Flask(__name__)
+
+
+def create_table():
+    connection = psycopg.connect(
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+    )
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+                CREATE TABLE IF NOT EXISTS messages (
+                    id SERIAL PRIMARY KEY,
+                    message TEXT NOT NULL,
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )
+                """
+            )
+
 
 @app.route("/hello")
 def hello():
@@ -8,4 +34,5 @@ def hello():
 
 
 if __name__ == "__main__":
+    create_table()
     app.run(host="0.0.0.0", port=5000)
